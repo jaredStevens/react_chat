c4a510e48e56f240e3a77ea8e0146093d7db2db2
Jared Stevens <jared.b.stevens@gmail.com>
2018-07-17 16:59:01 +0000

    server app.js

--- server/app.js
+++ server/app.js
@@ -1,52 +1,61 @@
+const express = require('express');
 const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8989 })
-
-const users = []
-
+const wss = new WebSocket.Server({ port: 8989 });
+const app = express();
+const PORT = process.env.PORT || 3000;
+	app.set("port", PORT);
+const users = [];
 const broadcast = (data, ws) => {
 	wss.clients.forEach((client) => {
 		if (client.readyState === WebSocket.OPEN && client !== ws ) { 
-			client.send(JSON.stringify(data))
+			client.send(JSON.stringify(data));
 		}
-	})
-}
+	});
+};
 
 wss.on('connection', (ws) => {
-	let index
+	let index;
 	ws.on('message', (message) => {
-		const data = JSON.parse(message)
+		const data = JSON.parse(message);
 		switch (data.type) {
 			case 'ADD_USER': {
-				index = users.length
-				users.push({ name: data.name, id: index + 1})
+				index = users.length;
+				users.push({ name: data.name, id: index + 1});
 				ws.send(JSON.stringify({
 					type: 'USERS_LIST',
 					users
-				}))
+				}));
 				broadcast({
 					type: 'USERS_LIST',
 					users
-				}, ws)
-				break
+				}, ws);
+				break;
 			}
 			case 'ADD_MESSAGE':
 				broadcast({
 					type: 'ADD_MESSAGE',
 					message: data.message,
 					author: data.author
-				}, ws)
-				break
+				}, ws);
+				break;
 			default:
-				break
+				break;
 		}
-	})
+	});
 
 	ws.on('close', () => {
-		users.splice(index, 1)
+		users.splice(index, 1);
 		broadcast({
 			type: 'USERS_LIST',
 			users
-		}, ws)
-	})
-})
+		}, ws);
+	});
+});
+
+app.listen(PORT, function(){
+	console.log("\n\n===== listening for requests on port " + PORT + " =====\n\n");
+});
+
+app.get('*', function(req, res, next){
+	res.sendFile(__dirname + "/public/views/index.html");
+});
